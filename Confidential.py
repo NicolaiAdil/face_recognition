8f02420cee973bd528ceab74d709e6885b57eb6e
Nicolai <nicolaiadil.aatif@gmail.com>
2023-01-22 18:40:31 +0100

    Added more functionality incl discord bot

---
+++ Confidential.py
@@ -0,0 +1,10 @@
+# Camera stuff
+USERNAME = "admin"
+PASSWORD = "Defektmikro123"
+IP_ADRESS = "192.168.0.108"
+# IP_ADRESS = "192.168.10.105"
+
+# Discord stuff
+INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=1055543829215842364&permissions=1634235578432&scope=bot"
+TOKEN = "MTA1NTU0MzgyOTIxNTg0MjM2NA.GT61LP.RWfsqy6t4JUdwyrgKkEMYIOQINfPz4Lgk_I12w"
+WEBHOOK_URL = "https://discordapp.com/api/webhooks/1066751522282684487/g2rXGz96aZYLNnwU_plrgT5qb-KgHjLLBx8NFWNGL9_BCpzNGTRLs8QmExZJzC5pPa9i"
---
+++ bot.py
@@ -0,0 +1,15 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from Confidential import TOKEN, WEBHOOK_URL
+from datetime import datetime
+
+def send_image(image: str, number: int) -> None:
+    webhook = DiscordWebhook(url=WEBHOOK_URL)
+
+    with open("unknown.jpg", "rb") as f:
+        webhook.add_file(file=f.read(), filename='unknown.jpg')
+
+    embed = DiscordEmbed(title='Intruder', description=f'{number} intruder(s) spotted {datetime.now()}', color='03b2f8')
+    webhook.add_embed(embed)
+
+
+    response = webhook.execute()
